3ae1fa5e743a9763e7ac0d90febec1c7c7085615
Leonardo da Silva Francisco <leonardo.francisco@bt.digital>
2023-04-07 18:05:29 -0300

    Implantação inicial

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
     <div className='fixed z-50 bg-dark-500 w-full top-0 left-0 px-8 py-4 lg:px-20 xl:px-36'>
         <div className="flex justify-between items-center text-white">
-            <img src={logo} className="App-logo w-10 rounded-full" alt="logo" style={{width:180,height:100}} />
+            <img src={logo} className="App-logo w-10 " alt="logo" style={{width:180,height:100}} />
             <ul className="hidden md:flex">
             <li className="p-4"><a href="#home" className="hover:underline">About</a></li>
             <li className="p-4"><a href="#skills" className="hover:underline">Skills</a></li>
