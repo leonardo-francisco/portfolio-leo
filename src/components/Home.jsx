bce12adc7b669ec337064e7230213a7bc4b44d0e
Leonardo da Silva Francisco <leonardo.francisco@bt.digital>
2023-04-11 10:00:07 -0300

    subida op

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import {useState} from 'react'
 
 import profile from '../assets/profile.svg';
 import me from '../assets/me.svg';
+import csharp from '../assets/csharppiece.svg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faCircleArrowRight, } from "@fortawesome/free-solid-svg-icons";
 import {  faGithub, faInstagram, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
@@ -24,9 +25,9 @@ export default function Hiro () {
         }
         <div id="home" className="flex w-full h-screen flex-col md:flex-row gap-5 items-center justify-center text-white relative">
             <div className='md:w-3/6 md:p-4'>
-                <img src={me} alt="me" className="App-logo w-6 rounded-full" onLoad={() => setLoaded(false)} style={{width:239, height:253, border: '5px solid gold', borderColor:'#9b870c', background:'linear-gradient(#eeaeca, #3a618f)'}} />
-                {/* <img data-aos="flip-right" data-aos-duration="1500" data-aos-offset="200"  src={profile} alt="profile" onLoad={() => setLoaded(false)}
-                   style={{width:739, height:353}} /> */}
+                {/* <img src={me} alt="me" className="App-logo w-6 rounded-full" onLoad={() => setLoaded(false)} style={{width:239, height:253, border: '5px solid gold', borderColor:'#9b870c', background:'linear-gradient(#eeaeca, #3a618f)'}} /> */}
+                <img data-aos="flip-right" data-aos-duration="1500" data-aos-offset="200"  src={csharp} alt="profile" onLoad={() => setLoaded(false)}
+                   style={{width:739, height:353}} />
             </div>
             <div className='md:w-3/6' data-aos="fade-right" data-aos-duration="1000" data-aos-offset="100" >
                 <div className="flex flex-col w-full mt-8">
